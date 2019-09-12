dfd075b40141688330ba6b6acf690c058dbdf773
Youngjung Yoon <youngjungyoon@Youngjungs-MacBook-Air.local>
2019-09-11 19:39:06 -0700

    add adp analysis tools and notebook

---
+++ read_help.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+# helps read in adp csv files
+def read_adp_csv(year):
+	return pd.read_csv(f'data/std_adp/adp_{year}.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
